6bd3dfa17c9a63154fdc2af319e3c4ab82719c97
BrianGoldYT <brianldg92@gmail.com>
2020-12-11 18:34:21 +0000

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 if [ "$(ping -c1 8.8.8.8)" ]
-then sudo apt-get install python3 && sudo chmod +x installer.py && sudo python3 installer.py
+then sudo apt-get install python3 git -y && sudo chmod +x installer.py && sudo python3 installer.py
 else echo -e "Es necesario una conexion a internet\nPorfavor compruebe su conexión"
 fi
